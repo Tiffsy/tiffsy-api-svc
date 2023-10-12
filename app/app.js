a3abd6b71a994262d7cf5070a1b384fb504ba41d
Raj Chordia <rajchordia@Rajs-MacBook-Pro.local>
2023-10-12 14:15:44 +0530

    initial commit

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const serverless = require('serverless-http')
 const app = express()
-app.get('/hello', (req, res) => res.send('Hello World 1!'))
+app.get('/hello', (req, res) => res.send('Hello World 2!'))
 app.listen(3000, ()=> console.log('listening on Port 3000!'))
 
 module.exports.handler = serverless(app)
